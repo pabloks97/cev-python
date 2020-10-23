5a6d6f72cdc48317fa3d5c874178359fc81f4c72
Pablo Carracci <pablocarracci@gmail.com>
2020-10-23 08:09:34 -0300

    adicionada solução ex100

---
+++ mundo-3/ex100.py
@@ -0,0 +1,24 @@
+# Exercício 100 - Funções para Sortear e Somar
+
+from random import randint
+from utils import é_par
+
+def main():
+    print('Sorteando 5 valores da lista: ', end='')
+    valores = sorteia(5)
+    for v in valores: print(f'{v} ', end='')
+    print('PRONTO!')
+    print(f'Somando os valores pares de {valores}, temos {soma_pares(valores)}')
+
+
+def sorteia(n):
+    '''Retorna uma lista com n valores aleatórios entre 1 e 10.'''
+    return [randint(1, 10) for i in range(n)]
+
+def soma_pares(valores):
+    '''Retorna a soma dos valores pares.'''
+    return sum([v for v in valores if é_par(v)])
+
+
+# --------------------------------------------
+main()
