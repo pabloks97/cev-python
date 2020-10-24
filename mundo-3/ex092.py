310e46e2bf36455f4f5fbb7c0bcc9e4c8195c6aa
Pablo Carracci <pablocarracci@gmail.com>
2020-10-24 12:40:03 -0300

    adicionada solução ex092

---
+++ mundo-3/ex092.py
@@ -0,0 +1,18 @@
+# Exercício 092 - Cadastro de Trabalhador em Python
+
+from utils import calcula_idade
+from datetime import datetime
+ano_atual = datetime.now().year
+
+dicio = {}
+dicio['nome'] = input('Nome: ')
+dicio['idade'] = calcula_idade(int(input('Ano de Nascimento: ')))
+dicio['ctps'] = int(input('Carteira de Trabalho (0 não tem): '))
+if dicio.get('ctps') > 0:
+    dicio['contratação'] = int(input('Ano de Contratação: '))
+    dicio['salário'] = float(input('Salário: '))
+    dicio['aposentadoria'] = dicio['idade'] + ((dicio['contratação'] + 35) - ano_atual)
+print(20 * '-=')
+
+for k, v in dicio.items():
+    print(f' - {k} tem o valor {v}')
