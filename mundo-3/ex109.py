2d41f83cafc56b16e8982606a3c56d49b61671e2
Pablo Carracci <pablocarracci@gmail.com>
2020-10-25 17:57:15 -0300

    adicionada solução ex109

---
+++ mundo-3/ex109.py
@@ -0,0 +1,8 @@
+# Exercício 109 - Formatando Moedas em Python
+
+from moeda import moeda, metade, dobro, aumenta
+
+preço = float(input('Digite o preço: R$'))
+print(f'A metade de {moeda(preço)} é {moeda(metade(preço))}')
+print(f'O dobro de {moeda(preço)} é {moeda(dobro(preço))}')
+print(f'Aumentando 10%, temos {moeda(aumenta(10, preço))}')
