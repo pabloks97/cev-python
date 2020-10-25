d5ce4caee48568a0738aaabf82b9bb02c6a1f486
Pablo Carracci <pablocarracci@gmail.com>
2020-10-25 17:35:11 -0300

    adicionada solução ex104

---
+++ mundo-3/ex104.py
@@ -0,0 +1,19 @@
+# Exercício 104 - Validando Entrada de Dados em Python
+
+def main():
+    x = lê_int('Digite um número: ',
+               '\033[0;31mERRO! Digite um número inteiro válido: \033[m')
+    print(f'Você acabou de digitar o número {x}')
+
+
+def lê_int(prompt='', erro=''):
+    '''Lê um número inteiro do usúario.'''
+    while True:
+        x = input(prompt)
+        if x.isdigit() or (len(x) and x[0] == '-' and x[1:].isdigit()):
+            return int(x)
+        prompt = erro
+
+
+# ----------------------------------------------------
+main()
